16da030b8c9029bef5e9b0a8eb0feb4b790ad58b
matiasjuy19 <matiasjuy19@hotmail.com>
2013-04-13 18:40:32 -0300

    algo

---
+++ src/parroquia/Parroquia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parroquia;
+
+/**
+ *
+ * @author dario
+ */
+public class Parroquia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
